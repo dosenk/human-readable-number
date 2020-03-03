4b22e134769121ca583b56552cdfedfc0269b635
dosenk <eympel@gmail.com>
2020-03-03 12:59:49 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,87 @@
+<<<<<<< HEAD
+// module.exports = 
+function toReadable() {
+    let numbers = {
+        0: "zero",
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        21: "twenty-one",
+        22: "twenty-two",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety",
+        100: "one hundred",
+        1000: "one thousand"
+    }
+    console.log(numbers[0]);
+};
+toReadable();
+=======
 module.exports = function toReadable (number) {
-  
-}
+        let numbers = {
+            0: "zero",
+            1: "one",
+            2: "two",
+            3: "three",
+            4: "four",
+            5: "five",
+            6: "six",
+            7: "seven",
+            8: "eight",
+            9: "nine",
+            10: "ten",
+            11: "eleven",
+            12: "twelve",
+            13: "thirteen",
+            14: "fourteen",
+            15: "fifteen",
+            16: "sixteen",
+            17: "seventeen",
+            18: "eighteen",
+            19: "nineteen",
+            20: "twenty",
+            30: "thirty",
+            40: "forty",
+            50: "fifty",
+            60: "sixty",
+            70: "seventy",
+            80: "eighty",
+            90: "ninety",
+
+        }
+        if (number < 20) {
+            return numbers[number];
+        } else if (String(number).length == 2) {
+            let ost = number % 10;
+            let dec = number - ost;
+            return ost == 0 ? numbers[dec] : numbers[dec] + ' ' + numbers[ost];
+        } else if (String(number).length == 3) {
+            let ost = (number % 100) % 10;
+            let dec = (number % 100);
+            let sot = Number(String(number)[0]);
+            return numbers[sot] += dec == 0 ? ' hundred' : (dec - ost) < 20 ?  ' hundred ' + numbers[dec] : ' hundred ' + (ost == 0 ? numbers[dec] : numbers[dec - ost] + ' ' + numbers[ost]);
+        }
+};
+>>>>>>> c1586538bc59b0bc7b8fc9a0968a5cc38f2a8af8
