06066ba92d8518c0d45c76e95d0e041256f45448
dosenk <eympel@gmail.com>
2020-03-04 10:32:52 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -28,37 +28,6 @@ module.exports = function toReadable(number) {
         70: "seventy",
         80: "eighty",
         90: "ninety",
-
-module.exports = function toReadable (number) {
-        let numbers = {
-            0: "zero",
-            1: "one",
-            2: "two",
-            3: "three",
-            4: "four",
-            5: "five",
-            6: "six",
-            7: "seven",
-            8: "eight",
-            9: "nine",
-            10: "ten",
-            11: "eleven",
-            12: "twelve",
-            13: "thirteen",
-            14: "fourteen",
-            15: "fifteen",
-            16: "sixteen",
-            17: "seventeen",
-            18: "eighteen",
-            19: "nineteen",
-            20: "twenty",
-            30: "thirty",
-            40: "forty",
-            50: "fifty",
-            60: "sixty",
-            70: "seventy",
-            80: "eighty",
-            90: "ninety",
     }
 
     let ost, dec, sot = 0;
@@ -77,6 +46,4 @@ module.exports = function toReadable (number) {
         let res = numbers[sot].concat(' hundred');
         return res += dec == 0 ? '' : (dec - ost) < 20 ? ' ' + numbers[dec] : ' ' + (ost == 0 ? numbers[dec] : numbers[dec - ost] + ' ' + numbers[ost]);
     }
-    // return res += number < 20 ? numbers[number] : 
 };
-
